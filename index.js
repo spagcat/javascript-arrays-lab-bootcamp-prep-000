1c7c0eec216f2c9839ce43c83e988f8704e66bf7
Shel Orock <sdo243@nyu.edu>
2018-04-02 20:42:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name) {
+  kittens;
+  kittens.push(name);
+}
+
+destructivelyAppendKitten('Ralph');
+
+function destructivelyPrependKitten(name) {
+  kittens;
+  kittens.unshift(name);
+}
+
+destructivelyPrependKitten('Bob');
+
+function destructivelyRemoveLastKitten() {
+  kittens;
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens;
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  kittens;
+  var newKittens = kittens.push(name);
+  return newKittens;
+}
+
+appendKitten('Broom');
+
+function prependKitten(name) {
+  kittens;
+  var newKittens = kittens.unshift(name);
+  return newKittens;
+}
+
+prependKitten('Arnold');
